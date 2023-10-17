3e4a5ae7b23f38056cf7d4f9b53bbbd609703fa4
Rabillon Thomas <o22201592@info03-31.iut45.univ-orleans.fr>
2023-10-17 13:57:34 +0200

    ajout d'insert de test

--- App/views.py
+++ App/views.py
@@ -8,9 +8,12 @@ import datetime
 def home():
     return render_template('home.html')
 
-@app.route('/posts')
+@app.route('/test')
 def posts():
     UserDB.insert_new_user("pseudo", "nom", "prenom", "email", "password")
+    UserDB.insert_new_user("pseudo1", "nom", "prenom", "email", "password")
+    UserDB.insert_new_user("pseudo2", "nom", "prenom", "email", "password")
+    UserDB.insert_new_user("pseudo3", "nom", "prenom", "email", "password")
     PostDB.insert_new_post(1, "titre", "c1", datetime.datetime.now(), UserDB.get_user_by_pseudo("pseudo"))
     PostDB.insert_new_post(2, "titre", "c2", datetime.datetime.now(), UserDB.get_user_by_pseudo("pseudo"))
     PostDB.insert_new_post(3, "titre", "c3", datetime.datetime.now(), UserDB.get_user_by_pseudo("pseudo"))
